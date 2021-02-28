2a6d13295806ccc1b71542e0fd96e27e300b1bd6
MarkOstapchuk <markostapchuk@gmail.com>
2021-02-28 09:05:22 +0300

    header

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    dots: true,
+    arrows: true,
+    nextArrow: '<div class="icon-cheveron-right"></div>',
+    prevArrow: '<div class="icon-cheveron-left"></div>',
+  })
+})
